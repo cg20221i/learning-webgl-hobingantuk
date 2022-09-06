32802a19cc9324f2b74697eda79a7cb365030777
Bagus Nugra <bagusnugrahaxd@gmail.com>
2022-09-06 14:52:05 +0700

    Fixing JS

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-function main(){
+function main() {
     var canvas = document.getElementById("myCanvas");
-}
+    var gl = canvas.getContext("webgl");
+  
+    // VERTEX SHADER
+    var vertexShaderCode = "void main () {" + "}";
+  
+    // FRAGMENT SHADER
+    var fragmentShaderCode = `
+          void main () {
+              
+          }
+    `;
+  }
