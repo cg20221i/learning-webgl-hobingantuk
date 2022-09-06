b3429d484703065d79a9756a2e189ea661c5a564
Bagus Nugra <bagusnugrahaxd@gmail.com>
2022-09-06 15:15:19 +0700

    Add color pink

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ function main() {
   
     // VERTEX SHADER
     var vertexShaderCode = "void main () {" + "}";
+
+    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShader,vertexShaderCode);
+    gl.compileShader(vertexShader);
+
   
     // FRAGMENT SHADER
     var fragmentShaderCode = `
@@ -11,4 +16,21 @@ function main() {
               
           }
     `;
+    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShader,fragmentShaderCode);
+    gl.compileShader(fragmentShader);
+
+    //Comparing to C-Programming, we may imagine
+    // that up to this step we have created two object files (.o),
+    // For the vertex and fragment shaders
+
+    var shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram,vertexShader);
+    gl.attachShader(shaderProgram,fragmentShader);
+    gl.linkProgram(shaderProgram);
+    gl.useProgram(shaderProgram);
+
+    gl.clearColor(1.0,0.75,0.79,1.0); // RGB and Alpha
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
   }
