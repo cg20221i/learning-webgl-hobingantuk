bf6c13f1c4ed1fc59706c8fc0df3e4e49d5a52df
Bagus Nugra <bagusnugrahaxd@gmail.com>
2022-09-13 14:49:11 +0700

    Draw Three Points

--- main.js
+++ main.js
@@ -2,11 +2,25 @@ function main() {
   var canvas = document.getElementById("myCanvas");
   var gl = canvas.getContext("webgl");
 
+  /**
+   * A(0.5,0.5)
+   * B(0.0,0.0)
+   * C(-0.5,0.5)
+   */
+
+  var vertices = [0.5, 0.5, 0.0, 0.0, -0.5, 0.5];
+
+  //Create a Linked-list for storing the vertices data in gpu realm
+  var buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
   // VERTEX SHADER
   var vertexShaderCode = `
+  attribute vec2 aPosition;
   void main () {
       gl_PointSize = 20.0;
-      gl_Position = vec4(0.5,0.5,1.0,1.0);
+      gl_Position = vec4(aPosition,0.0,1.0);
       //gl_position is the final destination for storing
       //positional data for the rendered vertex
 
@@ -20,6 +34,7 @@ function main() {
 
   // FRAGMENT SHADER
   var fragmentShaderCode = `
+          precision mediump float;
           void main () {
           gl_FragColor = vec4(0.0,0.0,1.0,1.0);  
           }
@@ -38,9 +53,16 @@ function main() {
   gl.linkProgram(shaderProgram);
   gl.useProgram(shaderProgram);
 
+  //Teach the GPU how to COllect
+  //the positional value from buffer
+  // for each vertex
+  var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
+  gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(aPosition);
+
   gl.clearColor(1.0, 0.75, 0.79, 1.0); // RGB and Alpha
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  gl.drawArrays(gl.POINT, 0, 1);
+  gl.drawArrays(gl.POINT, 0, 3);
   //   gl.drawElements()
 }
