4873d62248802b10478138653a20c2c7eae1cefe
Bagus Nugra <bagusnugrahaxd@gmail.com>
2022-09-13 13:57:53 +0700

    Draw 1 Point Basic

--- main.js
+++ main.js
@@ -1,36 +1,46 @@
 function main() {
-    var canvas = document.getElementById("myCanvas");
-    var gl = canvas.getContext("webgl");
-  
-    // VERTEX SHADER
-    var vertexShaderCode = "void main () {" + "}";
+  var canvas = document.getElementById("myCanvas");
+  var gl = canvas.getContext("webgl");
 
-    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vertexShader,vertexShaderCode);
-    gl.compileShader(vertexShader);
+  // VERTEX SHADER
+  var vertexShaderCode = `
+  void main () {
+      gl_PointSize = 20.0;
+      gl_Position = vec4(0.5,0.5,1.0,1.0);
+      //gl_position is the final destination for storing
+      //positional data for the rendered vertex
 
-  
-    // FRAGMENT SHADER
-    var fragmentShaderCode = `
+
+   }
+  `;
+
+  var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  gl.shaderSource(vertexShader, vertexShaderCode);
+  gl.compileShader(vertexShader);
+
+  // FRAGMENT SHADER
+  var fragmentShaderCode = `
           void main () {
-              
+          gl_FragColor = vec4(0.0,0.0,1.0,1.0);  
           }
     `;
-    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(fragmentShader,fragmentShaderCode);
-    gl.compileShader(fragmentShader);
+  var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.shaderSource(fragmentShader, fragmentShaderCode);
+  gl.compileShader(fragmentShader);
 
-    //Comparing to C-Programming, we may imagine
-    // that up to this step we have created two object files (.o),
-    // For the vertex and fragment shaders
+  //Comparing to C-Programming, we may imagine
+  // that up to this step we have created two object files (.o),
+  // For the vertex and fragment shaders
 
-    var shaderProgram = gl.createProgram();
-    gl.attachShader(shaderProgram,vertexShader);
-    gl.attachShader(shaderProgram,fragmentShader);
-    gl.linkProgram(shaderProgram);
-    gl.useProgram(shaderProgram);
+  var shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
+  gl.useProgram(shaderProgram);
 
-    gl.clearColor(1.0,0.75,0.79,1.0); // RGB and Alpha
-    gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.clearColor(1.0, 0.75, 0.79, 1.0); // RGB and Alpha
+  gl.clear(gl.COLOR_BUFFER_BIT);
 
-  }
+  gl.drawArrays(gl.POINT, 0, 1);
+  //   gl.drawElements()
+}
